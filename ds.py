f7811d39e226d512d483b53b0016e82898022c10
Lukas Sparrow <lukas.sparrow@gmail.com>
2015-06-27 20:46:09 +0200

    ds.py added

---
+++ ds.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2
+
+
+def decorrstretch(A, tol=None):
+    """
+    Apply decorrelation stretch to image
+
+    Arguments:
+    A   -- image in cv2/numpy.array format
+    tol -- upper and lower limit of contrast stretching
+    """
+
+    # save the original shape
+    orig_shape = A.shape
+    # reshape the image
+    #         B G R
+    # pixel 1 .
+    # pixel 2   .
+    #  . . .      .
+    A = A.reshape((-1,3)).astype(np.float)
+    # covariance matrix of A
+    cov = np.cov(A.T)
+    # source and target sigma
+    sigma = np.diag(np.sqrt(cov.diagonal()))
+    # eigen decomposition of covariance matrix
+    eigval, V = np.linalg.eig(cov)
+    # stretch matrix
+    S = np.diag(1/np.sqrt(eigval))
+    # compute mean of each color
+    mean = np.mean(A, axis=0)
+    # substract the mean from image
+    A -= mean
+    # compute the transformation matrix
+    T = reduce(np.dot, [sigma, V, S, V.T])
+    # compute offset 
+    offset = mean - np.dot(mean, T)
+    # transform the image
+    A = np.dot(A, T)
+    # add the mean and offset
+    A += mean + offset
+    # restore original shape
+    B = A.reshape(orig_shape)
+    # for each color...
+    for b in range(3):
+        # apply contrast stretching if requested
+        if tol:
+            # find lower and upper limit for contrast stretching
+            low, high = np.percentile(B[:,:,b], 100*tol), np.percentile(B[:,:,b], 100-100*tol)
+            B[B<low] = low
+            B[B>high] = high
+        # ...rescale the color values to 0..255
+        B[:,:,b] = 255 * (B[:,:,b] - B[:,:,b].min())/(B[:,:,b].max() - B[:,:,b].min())
+    # return it as uint8 (byte) image
+    return B.astype(np.uint8)
